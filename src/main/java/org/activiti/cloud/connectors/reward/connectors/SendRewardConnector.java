8ac804339ce029da84ee540b030b0a589068c7f1
Elias Ricken de Medeiros <erdemedeiros@users.noreply.github.com>
2018-09-21 09:57:22 +0200

    Update dependencies after pom refactoring (#4)
    - update groupIds
    
    - update package names

--- src/main/java/org/activiti/cloud/connectors/reward/connectors/SendRewardConnector.java
+++ src/main/java/org/activiti/cloud/connectors/reward/connectors/SendRewardConnector.java
@@ -1,25 +1,24 @@
 package org.activiti.cloud.connectors.reward.connectors;
 
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.activiti.cloud.api.process.model.IntegrationRequest;
+import org.activiti.cloud.api.process.model.IntegrationResult;
 import org.activiti.cloud.connectors.reward.model.RankedAuthor;
 import org.activiti.cloud.connectors.reward.model.Reward;
 import org.activiti.cloud.connectors.reward.services.RewardService;
 import org.activiti.cloud.connectors.starter.channels.IntegrationResultSender;
 import org.activiti.cloud.connectors.starter.configuration.ConnectorProperties;
 import org.activiti.cloud.connectors.starter.model.IntegrationResultBuilder;
-import org.activiti.runtime.api.model.IntegrationRequest;
-import org.activiti.runtime.api.model.IntegrationResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-import java.util.Collection;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 @EnableBinding(RewardMessageChannels.class)
@@ -38,28 +37,29 @@ public class SendRewardConnector {
     }
 
     @StreamListener(value = RewardMessageChannels.REWARD_CONSUMER)
-    public void rewardTopRankedUsers(IntegrationRequest event) throws IOException {
+    public void rewardTopRankedUsers(IntegrationRequest event) {
 
         Collection winners = (Collection) event.getIntegrationContext().getInBoundVariables().get("top");
         String campaign = String.valueOf(event.getIntegrationContext().getInBoundVariables().get("campaign"));
 
         for (Object winner : winners) {
             RankedAuthor rankedAuthor = mapper.convertValue(winner,
-                    RankedAuthor.class);
+                                                            RankedAuthor.class);
 
             Reward reward = new Reward(campaign,
-                    rankedAuthor,
-                    "You Won " + rankedAuthor.getUserName() + " for your participation in " + campaign,
-                    new Date());
+                                       rankedAuthor,
+                                       "You Won " + rankedAuthor.getUserName() + " for your participation in " + campaign,
+                                       new Date());
             rewardService.addNewRewardToCampaing(campaign,
-                    reward);
+                                                 reward);
         }
 
         Map<String, Object> results = new HashMap<>();
         results.put("rewards",
-                rewardService.getRewardsByCampaign(campaign,
-                        5));
-        Message<IntegrationResult> message = IntegrationResultBuilder.resultFor(event, connectorProperties)
+                    rewardService.getRewardsByCampaign(campaign,
+                                                       5));
+        Message<IntegrationResult> message = IntegrationResultBuilder.resultFor(event,
+                                                                                connectorProperties)
                 .withOutboundVariables(results)
                 .buildMessage();
 
